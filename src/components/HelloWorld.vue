94c2cb03dbedbcbfc1ce1f1ad91b31f8c110d072
FullstackRakibul <95490833+FullstackRakibul@users.noreply.github.com>
2024-08-19 15:15:52 +0600

    tailwind and antd design added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,16 @@ const submitTextValue = (event: Event) => {
 
 
 <template>
-  <h2>{{ textvalue }}</h2>
-  <input v-model="textvalue" @keyup.enter="submitTextValue" />
+  <a-card title="Default size card" style="width: 500px">
+    <div class="flex flex-col items-center justify-cente">
+      <h2 class="text-2xl font-bold text-blue-600 mb-4">{{ textvalue }}</h2>
+      <!-- <input class="border border-gray-300 rounded p-2" v-model="textvalue" @keyup.enter="submitTextValue"
+      placeholder="Type and press Enter" /> -->
+
+      <a-textarea v-model:value="textvalue" @keyup.enter="submitTextValue"
+        placeholder="Autosize height based on content lines" auto-size />
+
+    </div>
+  </a-card>
 
 </template>
